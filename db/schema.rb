f22d10345fcec65f7f358bffc9d748d328f83898
sukesan-kakusan <keisukezeze@gmail.com>
2021-09-21 23:48:41 +0900

    add index and has_secure_password to UserModel

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_09_20_092122) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
