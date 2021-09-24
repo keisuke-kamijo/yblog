7ad6d90c25cd10f6c93a99cbb1250dc7dcf666fb
sukesan-kakusan <keisukezeze@gmail.com>
2021-09-24 15:42:54 +0900

    rubocop -A

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get    '/signup',  to: 'users#new'
+  get '/signup', to: 'users#new'
   resources :users
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
