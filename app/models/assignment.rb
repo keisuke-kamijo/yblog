02160798fd33ce46289e14ecce1ab3fd89ca5143
sukesan-kakusan <keisukezeze@gmail.com>
2021-10-26 00:45:44 +0900

    add validation to Assignment

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,7 @@
 class Assignment < ApplicationRecord
   belongs_to :list
   belongs_to :article
+  validates :list_id, presence: true
+  validates :article_id, presence: true
+  validates :rank, presence:true
 end
