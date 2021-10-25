8b972283779294bcfde52d479dfa82ff08b221a1
sukesan-kakusan <keisukezeze@gmail.com>
2021-10-25 23:50:36 +0900

    generate model List

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_105840) do
+ActiveRecord::Schema.define(version: 2021_10_25_145003) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2021_09_27_105840) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_placed"
+    t.integer "rank"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "taggings", force: :cascade do |t|
