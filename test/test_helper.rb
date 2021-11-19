d0995d8a9ae1c79d885970495a78a49b270b29c8
Keisuke Kamijo <keisukezeze@gmail.com>
2021-11-19 20:09:00 +0900

    リスト操作におけるログイン要求のテストを追加

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,13 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  def log_in_as(user, password:'foobar')
+    post login_path, params:{ session: {email: user.email, password: password} }
+  end
 end
